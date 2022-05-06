3587c927fe714c9f54268090b5724d8f430db8e5
Jared Mates <jaredmates45@gmail.com>
2022-05-05 20:16:36 -0700

    set up pug in app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var expressLayouts = require("express-ejs-layouts");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -21,14 +20,13 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(expressLayouts);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
